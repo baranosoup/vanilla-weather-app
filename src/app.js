bb796a8de50d558e25ce0de70aaf842743537811
baranosoup <95305948+baranosoup@users.noreply.github.com>
2022-04-04 15:23:39 -0500

    Created a new forecast function

--- src/app.js
+++ src/app.js
@@ -13,6 +13,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-3">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="media/partly-cloudy-day.svg" alt="" width="42" />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">77°</span>
+              <span class="weather-forecast-temperature-min">61°</span>
+            </div>
+          </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -75,3 +98,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("Dallas");
+displayForecast();
